6ea77becaefa53178733f44b516ef58e32bbd8c1
Sanjar <sanjarBakhramovich@gmail.com>
2023-08-16 15:49:08 +0500

    weatherAPI.go add

---
+++ weatherApi.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type WeatherResponse struct {
+	Main struct {
+		Temperature float64 `json:"temp"`     // Temperature in Kelvin
+		Humidity    int     `json:"humidity"` // Humidity in %
+	} `json:"main"`
+}
+
+func main() {
+	apiKey := "75686baa4ffc8898ad84046eaf112d8c"
+	cityName := "Nukus" // Replace with the name of your hometown
+
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", cityName, apiKey)
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	var weather WeatherResponse
+	err = json.Unmarshal(data, &weather)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	temperatureCelsius := weather.Main.Temperature - 273.15 // Convert Kelvin to Celsius
+	humidity := weather.Main.Humidity
+
+	fmt.Printf("Temperature in %s: %.2f °C\n", cityName, temperatureCelsius)
+	fmt.Printf("Humidity in %s: %d %%\n", cityName, humidity)
+}
