3f61468bdcdda7e0cbdee0451fbfe10239d31e54
Andrey Peshkov <dizl.andry96@gmail.com>
2023-09-08 15:54:51 +0500

    Базовый CRUD написан, нужно изменить подправить грамматическую логику

--- StructOfSlice/slicePckg/slice.go
+++ StructOfSlice/slicePckg/slice.go
@@ -48,3 +48,33 @@ func (s *StructOfArr) Add(num int) bool {
 	s.Length++
 	return true
 }
+
+func (s *StructOfArr) DeleteVal(value int) bool {
+	for i := 0; i < s.Length; i++ {
+		if s.Arr[i] == value {
+			for j := i; j < s.Length-1; j++ {
+				s.Arr[j] = s.Arr[j+1]
+			}
+			s.Length--
+			return true
+		}
+	}
+	return false
+}
+
+func (s *StructOfArr) GetSlice() []int {
+	slice := make([]int, s.Length)
+	for i := 0; i < s.Length; i++ {
+		slice[i] = s.Arr[i]
+	}
+	return slice
+}
+
+func (s *StructOfArr) Update(index, newVal int) bool {
+	if index < 0 || index >= s.Length {
+		return false
+	}
+
+	s.Arr[index] = newVal
+	return true
+}
